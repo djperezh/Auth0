742df20351dc9118fb518f2a1ef13e7f4a412be1
Dom Perez <djperezh@gmail.com>
2022-06-02 00:32:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+Auth0 Tenant Client/Application API
+=================
+
+## Requirements
+
+- Node (ver 14 recommended)
+- Axios
+- npm
+- yarn (recomended)
+
+## Technologies
+
+- [Auth0](https://auth0.com/docs/api/management/v2/#!/Clients/delete_clients_by_id)
+- [Slack bolt-js](https://api.slack.com/start/building/bolt-js)
+
+Project Structure
+------------
+
+- `app.js` contains the primary Bolt app. It imports the Bolt package (`@slack/bolt`) and starts the Bolt app's server. It's where you'll add your app's listeners.
+- `.env` is where you'll put your Slack app's authorization token and signing secret.
+
+## Dev SetUp
+
+- You need Auth0 Account and Slack Account (Free account are enough)
+- Clone [repository](https://github.com/djperezh/Auth0)
+- Update the values in the .env file using your own tokens 
+
+```bash
+SLACK_BOT_TOKEN=<SLACK_BOT_TOKEN>
+SLACK_SIGNING_SECRET=<SLACK_SECRET>
+API_URL=<YOUR_API_URL>
+MGMT_API_ACCESS_TOKEN=<YOUR_AUTH0_TOKEN>
+```
+
+- Install packages by running the following command:
+
+```bash
+yarn install
+```
+
+- Start the server. 
+> You can use [glitch](https://glitch.com/) which is very friendly. However you will need to copy the code manually to glitch's IDE
---
+++ app.js
@@ -0,0 +1,109 @@
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+const axios = require('axios');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+let endpoint = process.env.API_URL
+const bearerToken = process.env.MGMT_API_ACCESS_TOKEN
+
+// HTTP POST
+app.command("/create", async ({ command, ack, say }) => {
+    await ack();
+
+    const bodyParameters = {
+      name: `${command.text}`
+    }
+
+    const config = {
+      headers: { Authorization: `Bearer ${bearerToken}` }
+    };
+
+    axios.post(`${process.env.API_URL}`, bodyParameters, config)
+    .then((response) => {
+      console.log(`Tenant: ${response.data.tenant}, Client/Application ID: ${response.data.client_id}, Name: ${response.data.name}`);
+      say(`Tenant: ${response.data.tenant}, Client/Application ID: ${response.data.client_id}, Name: ${response.data.name}`)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// HTTP PATCH
+app.command("/update", async ({ command, ack, say }) => {
+    await ack();
+
+    const params = command.text.split(' ') // ID, NewName
+    
+    endpoint = `${endpoint}/${params[0]}`
+    
+    // Update name only for now
+    const bodyParameters = {
+      name: `${params[1]}`
+    }
+
+    const config = {
+      headers: { Authorization: `Bearer ${bearerToken}` }
+    };
+
+    axios.patch(`${endpoint}`, bodyParameters, config)
+    .then((response) => {
+      console.log(`Tenant: ${response.data.tenant}, Client/Application ID: ${response.data.client_id}, Name: ${response.data.name}`);
+      say(`Tenant: ${response.data.tenant}, Client/Application ID: ${response.data.client_id}, Name: ${response.data.name}`)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// HTTP DELETE
+app.command("/delete", async ({ command, ack, say }) => {
+      await ack();
+      
+      if (command.text) {
+        endpoint = `${endpoint}/${command.text}`
+      
+        const config = {
+          headers: { Authorization: `Bearer ${bearerToken}` }
+        };
+
+        axios.delete(`${endpoint}`, config)
+        .then((response) => {
+          say(`Tenant: ${response.data.tenant}, Client/Application: ${response.data.name}`);
+          console.log(`Tenant: ${response.data.tenant}, Removed Client/Application ID: ${command.text}`);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      }
+});
+
+// HTTP GET
+app.command("/retrieve", async ({ command, ack, say }) => {
+    await ack();
+
+    if (command.text) {
+      endpoint = `${endpoint}/${command.text}`
+    }
+
+    const config = {
+      headers: { Authorization: `Bearer ${bearerToken}` }
+    };
+
+    axios.get(`${endpoint}`, config)
+    .then((response) => {
+      say(`Tenant: ${response.data.tenant}, Client/Application: ${response.data.name}`);
+      console.log(`Tenant: ${response.data.tenant}, Client/Application ID: ${response.data.name}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ server is running!!!');
+})();
